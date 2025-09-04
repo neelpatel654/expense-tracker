b55bbd2a9d6049d895438cadb1f54e4164eecfd2
neelpatel654 <nealpatel2004@gmail.com>
2025-09-04 18:47:53 +0530

    worked on sign up

---
+++ frontend/expense-tracker/src/components/inputs/Input.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa6"
+
+const Input = ({value, onChange, label, placeholder, type}) => {
+    const [showPassword,setShowPassword] =useState(false);
+
+    const toggelShowPassword = () => {
+        setShowPassword(!showPassword)
+    }
+  return (
+    <div>
+        <label className='text-[13px] text-slate-800'>{label}</label>
+
+        <div className='input-box'>
+            <input 
+                value={value}
+                placeholder={placeholder}
+                onChange={(e)=> onChange(e)}
+                type={type == "password" ? showPassword ? 'text' : 'password' : type}
+                className='w-full bg-transparent outline-none'
+            />
+
+            {type === "password" && (
+                <>
+                {showPassword ? (
+                    <FaRegEye 
+                        size={22}
+                        className='text-primary cursor-pointer'
+                        onClick={() => toggelShowPassword()}
+                    />
+                ) : (
+                    <FaRegEyeSlash 
+                        size={22}
+                        className='text-slate-400 cursor-pointer'
+                        onClick={() => toggelShowPassword()}
+                    />
+                )}
+                </>
+            )} 
+        </div>
+    </div>
+  )
+}
+
+export default Input;
--- frontend/expense-tracker/src/pages/Auth/SignUp.jsx
+++ frontend/expense-tracker/src/pages/Auth/SignUp.jsx
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import AuthLayout from "../../components/layouts/AuthLayout";
+import { useNavigate, Link } from "react-router-dom";
+import Input from "../../components/inputs/Input";
+import { validateEmail } from "../../utils/helper";
+import ProfilePhotoSelector from "../../components/inputs/ProfilePhotoSelector";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [profilePic, setProfilePic] = useState(null);
+  const [fullName, setFullName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+  };
+  return (
+    <AuthLayout>
+      <div className="lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">Create an Account</h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">
+          Join us today by entering your details below.
+        </p>
+        <form onSubmit={handleSignUp}>
+
+          <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
+
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <Input
+              value={fullName}
+              onChange={({ target }) => setFullName(target.value)}
+              label="Full Name"
+              placeholder="Jhon Wick"
+              type="text"
+            />
+
+            <Input
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+              label="Email Address"
+              placeholder="jhon@example.com"
+              type="text"
+            />
+            <div className="col-span-2">
+              <Input
+                value={password}
+                onChange={({ target }) => setPassword(target.value)}
+                label="Password"
+                placeholder="Min 8 Characters"
+                type="password"
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+    </AuthLayout>
+  );
 };
 
 export default SignUp;
