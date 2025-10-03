2385f0b1cc0708841813dca7d9ba1d71da0cd7fe
neelpatel654 <nealpatel2004@gmail.com>
2025-10-03 18:27:25 +0530

    started working on dashboard

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import DashboardLayout from "../../components/layouts/DashboardLayout";
+import { useUserAuth } from "../../hooks/useUserAuth";
 
 const Home = () => {
-  return <div>Home</div>;
+  useUserAuth();
+  return (
+    <DashboardLayout activeMenu="Dashboard">
+      <div className="my-5 mx-auto">Home</div>
+    </DashboardLayout>
+  )
 };
 
 export default Home;
---
+++ frontend/expense-tracker/src/utils/data.js
@@ -0,0 +1,33 @@
+import {
+    LuLayoutDashboard,
+    LuHandCoins,
+    LuWalletMinimal,
+    LuLogOut
+} from "react-icons/lu";
+
+export const SIDE_MENU_DATA = [
+    {
+        id:"01",
+        lable:"Dashboard",
+        icon:LuLayoutDashboard,
+        path:"/dashboard"
+    },
+    {
+        id:"02",
+        lable:"Income",
+        icon:LuWalletMinimal,
+        path:"/income"
+    },
+    {
+        id:"03",
+        lable:"Expense",
+        icon:LuHandCoins,
+        path:"/expense"
+    },
+    {
+        id:"06",
+        lable:"Logout",
+        icon:LuLogOut,
+        path:"/dashboard"
+    },
+]
