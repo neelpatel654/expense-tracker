bc18a9aad6f20ca7ed9a43fcaefcdf7cacda261e
neelpatel654 <nealpatel2004@gmail.com>
2025-10-07 14:11:45 +0530

    worked on Home.jsx and started working on InfoCard.jsx

---
+++ frontend/expense-tracker/src/components/cards/InfoCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InfoCard = ({icon,lable,value,color}) => {
+  return (
+    <div>InfoCard</div>
+  )
+}
+
+export default InfoCard
--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -1,14 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DashboardLayout from "../../components/layouts/DashboardLayout";
 import { useUserAuth } from "../../hooks/useUserAuth";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
+import InfoCard from "../../components/cards/InfoCard";
+import { LuHandCoins,LuWalletMinimal } from "react-icons/lu";
+import {IoMdCard} from "react-icons/io";
 
 const Home = () => {
   useUserAuth();
+
+  const navigate = useNavigate();
+
+  const [dashboardData, setDashboardData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchDashboardData = async () => {
+    if (loading) return;
+    setLoading(true);
+
+    try {
+      const response = await axiosInstance.get(
+        `${API_PATHS.DASHBOARD.GET_DATA}`
+      );
+      
+      if (response.data) {
+        setDashboardData(response.data);
+      }
+      
+    } catch (e) {
+      console.log("Something went wrong. Please try again later.", e);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchDashboardData();
+    return () => {};
+  },[]);
+
   return (
     <DashboardLayout activeMenu="Dashboard">
-      <div className="my-5 mx-auto">Home</div>
+      <div className="my-5 mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          <InfoCard
+            icon={<IoMdCard />}
+            lable = "Total Balance"
+            value={addThousandsSeparator(dashboardData?.totalBalance || 0)}
+            color="bg-primary"
+          />
+        </div>
+      </div>
     </DashboardLayout>
-  )
+  );
 };
 
 export default Home;
