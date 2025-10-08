0e78ea26f498d51f86e737ca3296c655650eabd7
neelpatel654 <nealpatel2004@gmail.com>
2025-10-08 18:39:59 +0530

    working on dashboard

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -7,6 +7,8 @@ import { API_PATHS } from "../../utils/apiPaths";
 import InfoCard from "../../components/cards/InfoCard";
 import { LuHandCoins,LuWalletMinimal } from "react-icons/lu";
 import {IoMdCard} from "react-icons/io";
+import {addThousandsSeparator} from "../../utils/helper";
+import RecentTransactions from "../../components/dashboard/RecentTransactions";
 
 const Home = () => {
   useUserAuth();
@@ -50,6 +52,24 @@ const Home = () => {
             lable = "Total Balance"
             value={addThousandsSeparator(dashboardData?.totalBalance || 0)}
             color="bg-primary"
+          />
+          <InfoCard
+            icon={<LuWalletMinimal />}
+            lable = "Total Income"
+            value={addThousandsSeparator(dashboardData?.totalIncome || 0)}
+            color="bg-orange-500"
+          />
+          <InfoCard
+            icon={<LuHandCoins />}
+            lable = "Total Expense"
+            value={addThousandsSeparator(dashboardData?.totalExpense || 0)}
+            color="bg-red-500"
+          />
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
+          <RecentTransactions
+            transactions={dashboardData?.recentTransactions}
+            onSeeMore = {() => navigate("/expense")}
           />
         </div>
       </div>
--- frontend/expense-tracker/src/utils/helper.js
+++ frontend/expense-tracker/src/utils/helper.js
@@ -1,16 +1,27 @@
 export const validateEmail = (email) => {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(email);
-}
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+};
 
 export const getInitials = (name) => {
-    if(!name) return "";
+  if (!name) return "";
 
-    const words = name.split(" ");
-    let initials = "";
+  const words = name.split(" ");
+  let initials = "";
 
-    for (let i = 0; i < Math.min(words.length,2); i++) {
-        initials += words[i][0];
-    }
-    return initials.toUpperCase();
-}
+  for (let i = 0; i < Math.min(words.length, 2); i++) {
+    initials += words[i][0];
+  }
+  return initials.toUpperCase();
+};
+
+export const addThousandsSeparator = (num) => {
+  if (num == null || num == isNaN(num)) return "";
+
+  const [integerPart, fractionalPart] = num.toString().split(".");
+  const formattedInteger = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+  return fractionalPart
+    ? `${formattedInteger}.${fractionalPart}`
+    : formattedInteger;
+};
